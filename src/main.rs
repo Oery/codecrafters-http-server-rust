3348ad59817ebebb7f6c5a35f52c51e01c074b99
oery <reyzac457@gmail.com>
2024-07-11 15:41:44 +0200

    add user-agent route

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,42 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+fn send_text(stream: &mut TcpStream, text: &str) -> std::io::Result<()> {
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        text.len(),
+        text
+    );
+    stream.write_all(response.as_bytes())
+}
+
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let mut lines = buf_reader.lines();
+    let request_line = lines.next().unwrap().unwrap();
+
+    let _host_line = lines.next().unwrap().unwrap();
+    let user_agent_line = lines.next().unwrap().unwrap();
 
     println!("{}", request_line);
+    println!("{}", user_agent_line);
 
     let route = request_line.split(' ').nth(1).unwrap();
     let base_route = route.split('/').nth(1).unwrap();
-
-    println!("base_route: {}", base_route);
 
     match base_route {
         "" => {
             let response = "HTTP/1.1 200 OK\r\n\r\n";
             stream.write_all(response.as_bytes())?;
         }
+        "user-agent" => {
+            let user_agent = user_agent_line.split(": ").nth(1).unwrap();
+            send_text(&mut stream, user_agent)?;
+        }
         "echo" => {
             let message = route.split('/').nth(2).unwrap();
-            let length = message.len();
-            let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{message}");
-            stream.write_all(response.as_bytes())?;
+            send_text(&mut stream, message)?;
         }
         _ => {
             let response = "HTTP/1.1 404 Not Found\r\n\r\n";
