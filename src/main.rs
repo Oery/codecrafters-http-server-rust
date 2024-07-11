8f93725f48af8905e6ab225201b1a185ff78f6a9
oery <reyzac457@gmail.com>
2024-07-11 13:43:32 +0200

    add route handler

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
 use std::{
-    io::Write,
+    io::{prelude::*, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
 
-fn handle_connection(mut stream: TcpStream) {
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    match stream.write_all(response.as_bytes()) {
-        Ok(()) => {
-            println!("Sent 200");
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    println!("{}", request_line);
+
+    let route = request_line.split(' ').nth(1).unwrap();
+
+    match route {
+        "/" => {
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            stream.write_all(response.as_bytes())?;
         }
-        Err(e) => {
-            println!("error: {}", e);
+        _ => {
+            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            stream.write_all(response.as_bytes())?;
         }
     }
+
+    Ok(())
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
-                handle_connection(_stream);
+                handle_connection(_stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
