f97be51e66b4bfb83689c6468798caca0147dcee
oery <reyzac457@gmail.com>
2024-07-11 13:24:44 +0200

    add 200 response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) {
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    match stream.write_all(response.as_bytes()) {
+        Ok(()) => {
+            println!("Sent 200");
+        }
+        Err(e) => {
+            println!("error: {}", e);
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,6 +22,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
