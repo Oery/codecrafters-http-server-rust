1e492cad63fd3f695da4f02961907c7ec10a343a
oery <reyzac457@gmail.com>
2024-07-12 15:20:33 +0200

    add better request parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,73 @@
 use std::{
     env,
-    io::{prelude::*, BufReader, Write},
+    io::{prelude::*, Write},
     net::{TcpListener, TcpStream},
 };
+
+#[derive(Debug)]
+enum Method {
+    Get,
+    Post,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Request {
+    method: Method,
+    path: String,
+    user_agent: Option<String>,
+    content_type: Option<String>,
+    content_length: Option<usize>,
+    body: Option<String>,
+}
+
+impl Request {
+    fn parse(request_string: &str) -> std::io::Result<Self> {
+        let parts = request_string.split("\r\n\r\n").collect::<Vec<&str>>();
+        println!("{:?}", parts);
+
+        let request_line = parts.first().unwrap();
+        let method = request_line.split(' ').next().unwrap();
+        let path = request_line.split(' ').nth(1).unwrap();
+
+        let mut user_agent = None;
+        let mut content_type = None;
+        let mut content_length = None;
+
+        let headers = parts.get(1).unwrap();
+
+        for header in headers.split("\r\n") {
+            if let [key, value] = header.split(": ").collect::<Vec<&str>>()[..] {
+                match key {
+                    "User-Agent" => user_agent = Some(value.to_string()),
+                    "Content-Type" => content_type = Some(value.to_string()),
+                    "Content-Length" => content_length = Some(value.parse::<usize>().unwrap()),
+                    _ => {}
+                }
+            }
+        }
+
+        let body = parts.last().map(|s| s.to_string());
+
+        Ok(Self {
+            method: match method {
+                "GET" => Method::Get,
+                "POST" => Method::Post,
+                _ => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        format!("unknown method: {}", method),
+                    ))
+                }
+            },
+            path: path.to_string(),
+            user_agent,
+            content_type,
+            content_length,
+            body,
+        })
+    }
+}
 
 fn send_text(stream: &mut TcpStream, text: &str) -> std::io::Result<()> {
     let response = format!(
@@ -28,19 +93,10 @@ fn handle_connection(mut stream: TcpStream, directory: &str) -> std::io::Result<
     let bytes = stream.read(&mut request)?;
     let request_string = String::from_utf8_lossy(&request[..bytes]).into_owned();
 
-    println!("Serving files from directory: {}", directory);
+    let request = Request::parse(&request_string)?;
+    println!("{:?}", request);
 
-    let parts = request_string.split("\r\n\r\n").collect::<Vec<&str>>();
-    println!("{:?}", parts);
-    let request_and_headers = parts[0];
-    let headers = request_and_headers.split("\r\n").collect::<Vec<&str>>();
-
-    let request = headers.first().unwrap();
-    let user_agent_line = headers.get(2).unwrap();
-    let body = parts[1];
-
-    let route = request.split(' ').nth(1).unwrap();
-    let base_route = route.split('/').nth(1).unwrap();
+    let base_route = request.path.split('/').nth(1).unwrap();
 
     match base_route {
         "" => {
@@ -48,59 +104,51 @@ fn handle_connection(mut stream: TcpStream, directory: &str) -> std::io::Result<
             stream.write_all(response.as_bytes())?;
         }
         "user-agent" => {
-            let user_agent = user_agent_line.split(": ").nth(1).unwrap();
-            send_text(&mut stream, user_agent)?;
+            let user_agent = request.user_agent.unwrap();
+            send_text(&mut stream, &user_agent)?;
         }
         "echo" => {
-            let message = route.split('/').nth(2).unwrap();
+            let message = request.path.split('/').nth(2).unwrap();
             send_text(&mut stream, message)?;
         }
-        "files" => {
-            let method = request.split(' ').next().unwrap();
-            println!("{}", method);
-            match method {
-                "GET" => {
-                    let file = route.split('/').nth(2).unwrap();
-                    let path = format!("{}/{}", directory, file);
+        "files" => match request.method {
+            Method::Get => {
+                let file = request.path.split('/').nth(2).unwrap();
+                let path = format!("{}/{}", directory, file);
 
-                    let contents = match std::fs::read(path) {
-                        Ok(contents) => contents,
-                        Err(_) => {
-                            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                            stream.write_all(response.as_bytes())?;
-                            return Ok(());
-                        }
-                    };
-                    send_octet_stream(&mut stream, &contents)?;
-                }
-                "POST" => {
-                    let file = route.split('/').nth(2).unwrap();
-                    let path = format!("{}/{}", directory, file);
-
-                    let content = body.split('\r').next().unwrap();
-
-                    println!("Saving file to {}", path);
-
-                    let mut file = match std::fs::File::create(path) {
-                        Ok(file) => file,
-                        Err(_) => {
-                            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                            stream.write_all(response.as_bytes())?;
-                            return Ok(());
-                        }
-                    };
-                    file.write_all(content.as_bytes())?;
-                    let response = "HTTP/1.1 201 Created\r\n\r\n";
-                    stream.write_all(response.as_bytes())?;
-                    return Ok(());
-                }
-                _ => {
-                    let response = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
-                    stream.write_all(response.as_bytes())?;
-                    return Ok(());
-                }
+                let contents = match std::fs::read(path) {
+                    Ok(contents) => contents,
+                    Err(_) => {
+                        let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                        stream.write_all(response.as_bytes())?;
+                        return Ok(());
+                    }
+                };
+                send_octet_stream(&mut stream, &contents)?;
             }
-        }
+            Method::Post => {
+                let file = request.path.split('/').nth(2).unwrap();
+                let path = format!("{}/{}", directory, file);
+
+                let body = request.body.unwrap();
+                let content = body.split('\r').next().unwrap();
+
+                println!("Saving file to {}", path);
+
+                let mut file = match std::fs::File::create(path) {
+                    Ok(file) => file,
+                    Err(_) => {
+                        let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                        stream.write_all(response.as_bytes())?;
+                        return Ok(());
+                    }
+                };
+                file.write_all(content.as_bytes())?;
+                let response = "HTTP/1.1 201 Created\r\n\r\n";
+                stream.write_all(response.as_bytes())?;
+                return Ok(());
+            }
+        },
         _ => {
             let response = "HTTP/1.1 404 Not Found\r\n\r\n";
             stream.write_all(response.as_bytes())?;
